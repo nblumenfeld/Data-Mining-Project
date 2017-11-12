ed93ce82869c78b24dbfe419245184cb8c6fdb38
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2017-11-12 16:21:19 -0700

    setup initial dataframe

--- SoppProsject.py
+++ SoppProsject.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
 import math
+
+df = pd.read_csv('agaricus-lepiota.csv', header=None,
+                 names=['cap-shape', 'cap-surface', 'cap-color', 'bruises?', 'odor', 'gill-attachment', 'gill-spacing', 'gill-size', 'gill-color', 'stock-shape', 'stock-root', 'stock-surface-above-ring', 'stock-surface-below-ring', 'stock-color-above-ring','stock-color-below-ring', 'veil-type', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color', 'population','habitat'])
+
+print df.head(10)
