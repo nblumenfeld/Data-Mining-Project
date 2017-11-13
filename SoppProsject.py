9a0bfa62555a0bdd720c64a3948bd5337735685b
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2017-11-12 17:40:24 -0700

    Ian did some stuff and things

--- SoppProsject.py
+++ SoppProsject.py
@@ -10,7 +10,41 @@ df = pd.read_csv('agaricus-lepiota.csv', header=None,
 del df['stock-root']
 
 target = df['class']
-print target.as_matrix()
+#print target.as_matrix()
 del df['class']
 
-print df.head()
+#print df.head()
+
+data = df.as_matrix()
+
+
+bruises = data[:,3]
+bruises = data[:,3]
+bruisesDict = {}
+bruisesDict['t'] = 0
+bruisesDict['f'] = 0
+TDict = {'e':0, 'p':0}
+FDict = {'e':0, 'p':0}
+# print bruises
+for i in range(len(bruises)):
+    if bruises[i] == 't':
+        bruisesDict['t'] += 1
+        if target[i] == 'e':
+            TDict['e'] += 1
+        elif target[i] == 'p':
+            TDict['p'] += 1
+    elif bruises[i] == 'f':
+        bruisesDict['f'] += 1
+        if target[i] == 'e':
+            FDict['e'] += 1
+        elif target[i] == 'p':
+            FDict['p'] += 1
+
+print 'Bruises:', bruisesDict
+print 'True, poision/eatable: ', TDict
+print 'False, poision/eatble: ', FDict
+print '% Bruised and Poison: ', (float(TDict['p'])/len(bruises))*100
+print '% Bruised and Eatable: ', (float(TDict['e'])/len(bruises))*100
+print '% Not Bruised and Poison: ', (float(FDict['p'])/len(bruises))*100
+print '% Not Bruise and Eatable: ', (float(FDict['e'])/len(bruises))*100
+print 'total %: ', (float(TDict['p'])/len(bruises))*100+(float(TDict['e'])/len(bruises))*100+(float(FDict['p'])/len(bruises))*100+(float(FDict['e'])/len(bruises))*100
