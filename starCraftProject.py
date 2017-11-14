e674e7836e7f63f29ca098d1f8a65e493ed4e8d4
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2017-11-13 18:30:26 -0700

    First iteration of data trimming for first turn in

--- starCraftProject.py
+++ starCraftProject.py
@@ -11,7 +11,9 @@ Noah Blumenfeld
 
 import pandas as pd
 import numpy as np
+from sklearn import tree
 import math
+
 
 #read the csv file but exclude the first row
 #1140 rows
@@ -27,16 +29,44 @@ df = pd.read_csv(r'https://github.com/bgweber/StarCraftMining/raw/master/data/sc
 'ProtossArbitor','ProtossStatis','ProtossRecall','ProtossAirWeapons1','ProtossAirArmor1','ProtossAirWeapons2','ProtossAirArmor2','midBuild'])
 
 target = df['midBuild'].as_matrix()
+question1_sample = df[['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossGateway','ProtossGroundWeapons1','ProtossShields1','ProtossCitadel','midBuild']]
 df = df.iloc[:,:-1]
 data = df.as_matrix()
 name = df.columns.values
 
 
+# build_data = df.sample(frac=.8)
+#
+# test_data = df.loc[~df.index.isin(build_data.index)]
+
+stds = 1.0  # Number of standard deviation that defines 'outlier'.
+z = question1_sample[['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossGateway','ProtossGroundWeapons1','ProtossShields1','ProtossCitadel','midBuild']].groupby('midBuild').transform(
+    lambda group: (group - group.mean()).div(group.std()))
+outliers = z.abs() > stds
+
+data = question1_sample[outliers.any(axis=1)]
+
+build_data = data.sample(frac=.8)
+
+test_data = data.loc[~data.index.isin(build_data.index)]
+
+#print outliers
 
 
 
+build_data_labels = build_data['midBuild']
+test_data_labels = test_data['midBuild']
+build_data = build_data.iloc[:,:-1]
+test_data = test_data.iloc[:,:-1]
+data = build_data.as_matrix()
+test = test_data.as_matrix()
+name = df.columns.values
 
-print df.sample()
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(data,build_data_labels)
+print clf.predict(test)
+
+
 
 #print df.head(5)
 #print target
