9982a4c1b429ae170a232523d19167d076a62679
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2017-11-13 17:31:56 -0700

    Added in comments for how we should go about choosing and pruning data

--- starCraftProject.py
+++ starCraftProject.py
@@ -14,13 +14,29 @@ import numpy as np
 import math
 
 #read the csv file but exclude the first row
+#1140 rows
+#53 attributes
+
+#912 for build data if using full 1140, 228 for test
+#select 12 attributes at a time to build tree off of
+
+#obviously we need to change this after we trim out rows for outliers and missing/redundant data
+
 df = pd.read_csv(r'https://github.com/bgweber/StarCraftMining/raw/master/data/scmPvT_Protoss_Mid.csv', skiprows=1, header=None, names = ['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossSecondExpansion','ProtossThirdExpansion','ProtossFourthExpansion','ProtossGateway','ProtossSecondGatway','ProtossThirdGatway','ProtossFourthGatway','ProtossCore','ProtossZealot','ProtossGoon','ProtossRange','ProtossForge',
 'ProtossCannon','ProtossGroundWeapons1','ProtossGroundArmor1','ProtossShields1','ProtossGroundWeapons2','ProtossGroundArmor2','ProtossShields2','ProtossCitadel','ProtossLegs','ProtossArchives','ProtossTemplar','ProtossArchon','ProtossStorm','ProtossDarkTemplar','ProtossDarkArchon','ProtossMaelstorm','ProtossRoboBay','ProtossShuttle','ProtossShuttleSpeed','ProtossRoboSupport','ProtossReavor','ProtossReavorDamage','ProtossReavorCapacity','ProtossObservory','ProtossObs','ProtossStargate','ProtossCorsair','ProtossDisruptionWeb','ProtossFleetBeason','ProtossCarrier','ProtossCarrierCapacity','ProtossTribunal',
 'ProtossArbitor','ProtossStatis','ProtossRecall','ProtossAirWeapons1','ProtossAirArmor1','ProtossAirWeapons2','ProtossAirArmor2','midBuild'])
 
-
+target = df['midBuild'].as_matrix()
 df = df.iloc[:,:-1]
 data = df.as_matrix()
 name = df.columns.values
 
-print df.head(5)
+
+
+
+
+
+print df.sample()
+
+#print df.head(5)
+#print target
