35b3c59f70950de6976fa91228e1a371dae73327
ThatOnePandaGuy <noahwblumenfeld@gmail.com>
2017-11-19 15:36:17 -0700

    cleaned up code to make more sense

--- starCraftProject.py
+++ starCraftProject.py
@@ -33,37 +33,32 @@ df = pd.read_csv(r'https://github.com/bgweber/StarCraftMining/raw/master/data/sc
 'ProtossArbitor','ProtossStatis','ProtossRecall','ProtossAirWeapons1','ProtossAirArmor1','ProtossAirWeapons2','ProtossAirArmor2','midBuild'])
 
 target = df['midBuild'].as_matrix()
-# question1_sample = df[['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossGateway','ProtossGroundWeapons1','ProtossShields1','ProtossCitadel','midBuild']]
 df = df.iloc[:,:-1]
 data = df.as_matrix()
 name = df.columns.values
 
 
+'''
+All commented code below was what we did for step 1 of the project
+'''
+
+# question1_sample = df[['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossGateway','ProtossGroundWeapons1','ProtossShields1','ProtossCitadel','midBuild']]
 # build_data = df.sample(frac=.8)
-#
 # test_data = df.loc[~df.index.isin(build_data.index)]
 
-# The block of code below looks at our sample data and then based on the number of standard deviations we specifiy, in this case 1, will trim out outlier rows.
-# Since we chose 1 standard deviation as our cutoff, all the remaining data in the data set represents the average 95% of the set (Thomas check this number before you add it to the paper, I'm just going off what I remember from stats.)
-# Somehow the grouping is done be the labels that are associated with each row but I'm pretty sure it's looking at the other 9 columns as it's basis for what is an outlier or not.
+'''
+ The block of code below looks at our sample data and then based on the number of standard deviations we specifiy, in this case 1, will trim out outlier rows.
+ Since we chose 1 standard deviation as our cutoff, all the remaining data in the data set represents the average 95% of the set (Thomas check this number before you add it to the paper, I'm just going off what I remember from stats.)
+ Somehow the grouping is done be the labels that are associated with each row but I'm pretty sure it's looking at the other 9 columns as it's basis for what is an outlier or not.
+'''
 
 # stds = 1.0  # Number of standard deviation that defines 'outlier'.
 # z = question1_sample[['ProtossPylon','ProtossSecondPylon','ProtossFirstGas','ProtossSecondGas','ProtossFirstExpansion','ProtossGateway','ProtossGroundWeapons1','ProtossShields1','ProtossCitadel','midBuild']].groupby('midBuild').transform(
 #     lambda group: (group - group.mean()).div(group.std()))
 # outliers = z.abs() > stds
-
 # data = question1_sample[outliers.any(axis=1)]
-
 # build_data = data.sample(frac=.8)
-
 # test_data = data.loc[~data.index.isin(build_data.index)]
-
-#print outliers
-
-
-normalized_data_set = preprocessing.StandardScaler().fit_transform(data)
-
-
 # build_data_labels = build_data['midBuild']
 # test_data_labels = test_data['midBuild']
 # build_data = build_data.iloc[:,:-1]
@@ -85,5 +80,11 @@ normalized_data_set = preprocessing.StandardScaler().fit_transform(data)
 # classif_rate = np.mean(prediction.ravel() == test_data_labels.ravel()) * 100
 # print("classif_rate for %s : %f " % ('DecisionTree', classif_rate))
 
-#print df.head(5)
-#print target
+
+
+'''
+All code below relates to step 2 of the project
+'''
+normalized_data_set = preprocessing.StandardScaler().fit_transform(data)
+
+
